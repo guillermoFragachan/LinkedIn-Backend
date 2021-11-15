25bc74469a98c6e41911a1f135789db6e1be6d0e
guillermoFragachan <guillefragachan@gmail.com>
2021-11-15 14:04:26 +0100

    update

--- src/server.js
+++ src/server.js
@@ -1,34 +1,46 @@
-import cors from 'cors';
 import express from 'express';
-import mongoose from 'mongoose';
+import cors from 'cors';
 import listEndpoints from 'express-list-endpoints';
-import experienceRouter from './services/experience/index.js';
+import mongoose from "mongoose"
+import profileRouter from './services/profile/index.js'
+import postRouter from './services/post/index.js';
+
+
+
 
 const server = express();
 
-const port = process.env.PORT || 3001;
+
+const port = process.env.PORT || 3001
 
 // ********************************* MIDDLEWARES ***************************************
 
-server.use(cors());
-server.use(express.json());
+server.use(cors())
+server.use(express.json())
 
 // ********************************* ROUTES ********************************************
-server.use('/profile/username/experience', experienceRouter);
+
+server.use('/profile', profileRouter)
+server.use("/posts", postRouter)
 
 // ********************************* ERROR HANDLERS ************************************
 
-mongoose.connect(process.env.MONGO_CONNECTION);
 
-mongoose.connection.on('connected', () => {
-	console.log('Mongo Connected!');
 
-	server.listen(port, () => {
-		console.table(listEndpoints(server));
-		console.log(`✅ Server running on port ${port}`);
-	});
-});
 
-mongoose.connection.on('error', (err) => {
-	console.log(err);
-});
+mongoose.connect(process.env.MONGO_CONNECTION)
+
+mongoose.connection.on("connected", () => {
+    console.log("Mongo Connected!")
+
+
+server.listen(port, () => {
+    
+  console.table(listEndpoints(server))
+    console.log(`✅ Server running on port ${port}`)
+})
+})
+
+mongoose.connection.on("error", err => {
+  console.log(err)
+})
