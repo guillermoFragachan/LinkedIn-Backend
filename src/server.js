00d15b25dc1f9b8986e31b82da5e52659b78f7b2
adam-belahouane <adam_belahouane@outlook.com>
2021-11-15 09:15:13 +0000

    init

---
+++ src/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import cors from 'cors';
+import listEndpoints from 'express-list-endpoints';
+import mongoose from "mongoose"
+
+const server = express();
+
+const port = process.env.PORT || 3001
+
+// ********************************* MIDDLEWARES ***************************************
+
+server.use(cors())
+server.use(express.json())
+
+// ********************************* ROUTES ********************************************
+
+
+
+// ********************************* ERROR HANDLERS ************************************
+
+
+
+
+mongoose.connect(process.env.MONGO_CONNECTION)
+
+mongoose.connection.on("connected", () => {
+    console.log("Mongo Connected!")
+
+
+server.listen(port, () => {
+    
+  console.table(listEndpoints(server))
+    console.log(`✅ Server running on port ${port}`)
+})
+})
+
+mongoose.connection.on("error", err => {
+  console.log(err)
+})
