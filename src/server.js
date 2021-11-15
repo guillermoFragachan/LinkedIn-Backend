f6eee0f177cc1b8b2b923d242939af670cef021e
guillermoFragachan <guillefragachan@gmail.com>
2021-11-15 14:06:48 +0100

    add endpoitn abnet

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import listEndpoints from 'express-list-endpoints';
 import mongoose from "mongoose"
 import profileRouter from './services/profile/index.js'
 import postRouter from './services/post/index.js';
+import experienceRouter from './services/experience/index.js';
 
 
 
@@ -19,7 +20,7 @@ server.use(cors())
 server.use(express.json())
 
 // ********************************* ROUTES ********************************************
-
+server.use('/experience', experienceRouter)
 server.use('/profile', profileRouter)
 server.use("/posts", postRouter)
 
