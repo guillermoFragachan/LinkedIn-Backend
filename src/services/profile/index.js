b7f769a554f29883868fa012580d6983bd3ce110
guillermoFragachan <guillefragachan@gmail.com>
2021-11-15 11:35:11 +0100

    CRUD

--- src/services/profile/index.js
+++ src/services/profile/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import ProfileModel from "./sechema.js";
+import {getPDFReadableStream} from "./lib.js"
+import { pipeline } from "stream"
 
 
 const router = express.Router();
@@ -25,5 +27,70 @@ router.get('/', async (req, res, next) => {
     }
 })
 
+router.get('/:id', async (req, res, next) => {
+    try{
+        const profile = await ProfileModel.findById(req.params.id)
+        res.send(profile)
+    }catch (error){
+        next(error)
+    }
+})
+
+router.put('/:id', async (req, res, next) => {
+    try{
+        const profile = await ProfileModel.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        res.send(profile)
+    }catch (error){
+        next(error)
+    }
+})
+
+
+
+// *************** FILE ENDPOINTS ***********************
+
+router.put("./:id/picture", async (req, res, next) => {
+    try{
+        const profile = await ProfileModel.findByIdAndUpdate(req.params.id, {image: req.body.image}, {new: true})
+        res.send(profile)
+    }catch (error){
+        next(error)
+    }
+})
+
+router.get("/:id/CV",async (req, res, next) => {
+    try{
+    res.setHeader("Content-Disposition", "attachment; filename=CV.pdf")
+
+	const profile = await ProfileModel.findById(req.params.id)
+	// const profileIndex = profiles.findIndex(profile => profile._id === req.params.id)
+	
+	// const profile = profiles[profileIndex]
+
+	const source = getPDFReadableStream({ 
+		name: profile.name,
+		email: profile.email})
+		 // PDF READABLE STREAM
+    const destination = res
+
+    pipeline(source, destination, err => {
+      if (err) next(err)
+    })
+    }catch (error){
+        next(error)
+    }
+
+})
+
+
+//extra
+router.delete('/:id', async (req, res, next) => {
+    try{
+        await ProfileModel.findByIdAndDelete(req.params.id)
+        res.send(204)
+    }catch (error){
+        next(error)
+    }
+})
 
 export default router
