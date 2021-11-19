fef3d6fd06c59ba43c5cb6cf814698366b13b864
guillermoFragachan <guillefragachan@gmail.com>
2021-11-19 09:56:40 +0100

    fix comments

--- src/services/comments/index.js
+++ src/services/comments/index.js
@@ -4,8 +4,11 @@ import CommentModel from "./schema.js"
 
 
 const makeComment = async (req, res, next) => {
+
 	try {
+        req.body.post = req.params.postId;
 		const newComment = new CommentModel(req.body);
+
 		const { _id } = await newComment.save();
 		res.status(200).send(_id);
 	} catch (error) {
@@ -16,11 +19,14 @@ const makeComment = async (req, res, next) => {
 const getComments = async (req, res, next) => {
     try {
         const query = q2m(req.query);
-        const comments = await CommentModel.find().populate({path:"user", select:"name"}).populate({path:"post", select:"title"})
+        const comments = await CommentModel.find( {post: req.params.postId}).populate({path:"user", select:"name"}).populate({path:"post", select:"title"})
+        console.log(req.params.postId)
         res.status(200).send(comments);
     } catch (error) {
         next(error);
     }
+    //      const foundpost = await PostModel.find({username: friendsProfile.username})
+
 }
 
 const getSingleComment = async (req, res, next) => {
